ad7e14d2b4f603199ac7f07b74c7e1dc1a9f0ceb
Ivan Nemtsev <ixtreme3@yandex.ru>
2023-11-20 21:38:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.SystemClock
+import com.example.stopwatch.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var running = false
+    private var offset: Long = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        if (savedInstanceState != null) {
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if (running) {
+                binding.stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                binding.stopwatch.start()
+            } else setBaseTime()
+        }
+
+        binding.startButton.setOnClickListener {
+            if (!running) {
+                setBaseTime()
+                binding.stopwatch.start()
+                running = true
+            }
+        }
+
+        binding.pauseButton.setOnClickListener {
+            if (running) {
+                saveOffset()
+                binding.stopwatch.stop()
+                running = false
+            }
+        }
+
+        binding.resetButton.setOnClickListener {
+            offset = 0
+            setBaseTime()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (running) {
+            saveOffset()
+            binding.stopwatch.stop()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (running) {
+            setBaseTime()
+            binding.stopwatch.start()
+            offset = 0
+        }
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putLong(OFFSET_KEY, offset)
+        savedInstanceState.putBoolean(RUNNING_KEY, running)
+        savedInstanceState.putLong(BASE_KEY, binding.stopwatch.base)
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
+    private fun setBaseTime() {
+        binding.stopwatch.base = SystemClock.elapsedRealtime() - offset
+    }
+
+    private fun saveOffset() {
+        offset = SystemClock.elapsedRealtime() - binding.stopwatch.base
+    }
+
+    companion object {
+        private const val OFFSET_KEY = "offset"
+        private const val RUNNING_KEY = "running"
+        private const val BASE_KEY = "base"
+    }
+}
